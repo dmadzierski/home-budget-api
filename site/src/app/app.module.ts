7c7c6474ec3afc6bba989bbd29bb59329a2e3d39
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-09 15:36:46 +0200

    Add navigation

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import {RegisterComponent} from './register/register.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FooterComponent} from './footer/footer.component';
+
+const routes: Routes = [
+  {path: 'login-component', component: LoginComponent},
+  {path: 'register-component', component: RegisterComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
