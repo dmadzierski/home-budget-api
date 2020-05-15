a65d77f645ae32d3e64c0f89b60c20f0a0a40cb8
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-15 17:00:05 +0200

    Add new links to navigation, and authorize
    Took 41 seconds

--- site/src/app/navigation/navigation.component.ts
+++ site/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) {
+    auth.authenticate(undefined, undefined);
+  }
 
   ngOnInit(): void {
   }
 
+  authenticated() {
+    return this.auth.authenticated;
+  }
 }
