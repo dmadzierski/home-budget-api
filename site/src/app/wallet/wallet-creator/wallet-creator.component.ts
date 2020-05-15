cc9476d3f9eaef6d45368f3f2743bf313d5e79a9
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-15 17:11:59 +0200

    Created wallet formula page which will can add new wallet
    Took 1 minute

---
+++ site/src/app/wallet/wallet-creator/wallet-creator.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {Wallet} from '../../models/wallet.model';
+import {WalletHttpService} from '../wallet.http.service';
+
+@Component({
+  selector: 'app-wallet-creator',
+  templateUrl: './wallet-creator.component.html',
+  styleUrls: ['./wallet-creator.component.css']
+})
+export class WalletCreatorComponent implements OnInit {
+
+  constructor(private walletHttpService: WalletHttpService) {
+  }
+
+  wallet: Wallet = new Wallet();
+
+  ngOnInit(): void {
+  }
+
+  addWallet(){
+    this.walletHttpService.addWallet(this.wallet).subscribe(success => {
+        console.log(success);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+}
