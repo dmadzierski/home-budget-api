78d2e0af3273a18673b29fd13555f55a02bc3307
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-15 16:54:17 +0200

    Add "/user" uri to get check could user are authorize
    Took 9 hours 9 minutes

--- src/main/java/pl/BudgetHomeApplication.java
+++ src/main/java/pl/BudgetHomeApplication.java
@@ -2,7 +2,14 @@ package pl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
 @SpringBootApplication
 public class BudgetHomeApplication {
 
@@ -10,4 +17,8 @@ public class BudgetHomeApplication {
     SpringApplication.run(BudgetHomeApplication.class, args);
   }
 
+  @RequestMapping("/user")
+  public Principal user (Principal user) {
+    return user;
+  }
 }
