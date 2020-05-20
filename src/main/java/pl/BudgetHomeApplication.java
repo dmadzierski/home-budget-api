0ca5f8612161bb123b68fefa0ab91471d71ace9f
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-20 13:45:06 +0200

    Add and configure Swagger UI
    Took 2 hours 39 minutes

--- src/main/java/pl/BudgetHomeApplication.java
+++ src/main/java/pl/BudgetHomeApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.security.Principal;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
 @SpringBootApplication
+@EnableSwagger2
 public class BudgetHomeApplication {
 
   public static void main (String[] args) {
