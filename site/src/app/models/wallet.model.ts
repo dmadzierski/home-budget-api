4250e0a2c5b3b6d71e92b08151bad761797907a8
Oziaka <danile.madzierski.pl@gmail.com>
2020-05-25 10:09:18 +0200

    Divide app on site and api
    Took 43 minutes

--- site/src/app/models/wallet.model.ts
+++
@@ -1,8 +0,0 @@
-import {Transaction} from './transaction.model';
-
-export class Wallet {
-  public name: string;
-  public balance: bigint;
-  public id: bigint;
-  public transactions: Array<Transaction>;
-}
