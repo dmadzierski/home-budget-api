9e75092088ea355a15400216f44b69b1f6bc2cb8
Oziaka <daniel.madzierski.pl@gmail.com>
2022-12-20 04:08:44 +0100

    Created modules instead monolith

---
+++ java-clean-architecture/adapters/src/test/java/pl/AdaptersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ java-clean-architecture/domain/src/test/java/pl/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
